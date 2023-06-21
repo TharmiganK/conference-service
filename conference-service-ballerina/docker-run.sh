9cdeaaf329a0ce56e0b6926dbbb89f5c6c88c492
TharmiganK <ktharmi176@gmail.com>
2023-06-21 14:16:45 +0530

    Update scripts with date command

--- conference-service-ballerina/docker-run.sh
+++ conference-service-ballerina/docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "time = $(gdate +"%Y-%m-%dT%H:%M:%S.%3N")+05:30 level = INFO module = tharmigan/conference_service_ballerina message = Executing the Ballerina application"
+echo "time = $(date +"%Y-%m-%dT%H:%M:%S.%3N")+05:30 level = INFO module = tharmigan/conference_service_ballerina message = Executing the Ballerina application"
 docker run --name conference_service_ballerina --init -v ./Config.toml:/home/ballerina/Config.toml -p 8102:8102 --link country conference_service_ballerina:latest
--- conference-service-ballerina/run.sh
+++ conference-service-ballerina/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "time = $(gdate +"%Y-%m-%dT%H:%M:%S.%3N")+05:30 level = INFO module = tharmigan/conference_service_ballerina message = Executing the Ballerina application"
+echo "time = $(date +"%Y-%m-%dT%H:%M:%S.%3N")+05:30 level = INFO module = tharmigan/conference_service_ballerina message = Executing the Ballerina application"
 if [ "$1" = "graalvm" ];
 then
     ./target/bin/conference_service_ballerina
