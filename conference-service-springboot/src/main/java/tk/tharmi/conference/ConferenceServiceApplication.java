8192e73016af65cf1202607f8513dfd3d28c0a83
TharmiganK <ktharmi176@gmail.com>
2023-09-11 16:19:59 +0530

    Change application class name

--- conference-service-springboot/src/main/java/tk/tharmi/conference/ConferenceServiceApplication.java
+++ conference-service-springboot/src/main/java/tk/tharmi/conference/ConferenceServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConferenceServiceSpringbootApplication {
+public class ConferenceServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConferenceServiceSpringbootApplication.class, args);
+		SpringApplication.run(ConferenceServiceApplication.class, args);
 	}
 
 }
