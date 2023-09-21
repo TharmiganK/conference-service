f9989d4ddc4b3484e912b47651dbf12a38718d33
TharmiganK <ktharmi176@gmail.com>
2023-09-21 10:23:43 +0530

    Use gdate instead of date

--- ballerina/.scripts/run.sh
+++ ballerina/.scripts/run.sh
@@ -8,12 +8,12 @@ cp ../Config.toml ./Config.toml
 
 if [ "$1" = "graalvm" ];
 then
-    echo "time=$(date +"%Y-%m-%dT%H:%M:%S.%3N%:z") level=INFO module=tharmigan/conference_service_ballerina message=\"Executing the Ballerina application\"" >> ../.results/run-graalvm.txt
-    ../target/bin/conference_service_ballerina >> ../.results/run-graalvm.txt 2>&1
+    echo "time=$(gdate +"%Y-%m-%dT%H:%M:%S.%3N%:z") level=INFO module=tharmigan/conference_service_ballerina message=\"Executing the Ballerina application\"" >> ../.results/run-graalvm.txt
+    ../target/bin/conference_service_ballerina -Xmx512m >> ../.results/run-graalvm.txt 2>&1
     echo "" >> ../.results/run-graalvm.txt
 else
-    echo "time=$(date +"%Y-%m-%dT%H:%M:%S.%3N%:z") level=INFO module = tharmigan/conference_service_ballerina message=\"Executing the Ballerina application\"" >> ../.results/run-jvm.txt
-    java -jar ../target/bin/conference_service_ballerina.jar >> ../.results/run-jvm.txt 2>&1
+    echo "time=$(gdate +"%Y-%m-%dT%H:%M:%S.%3N%:z") level=INFO module=tharmigan/conference_service_ballerina message=\"Executing the Ballerina application\"" >> ../.results/run-jvm.txt
+    java -Xmx512m -jar ../target/bin/conference_service_ballerina.jar >> ../.results/run-jvm.txt 2>&1
     echo "" >> ../.results/run-jvm.txt
 fi
 
