d48efb421f8b9d0cba7bf33c907c24b081aad861
TharmiganK <ktharmi176@gmail.com>
2023-09-25 09:47:45 +0530

    Add bal persist command

--- ballerina/.scripts/build.sh
+++ ballerina/.scripts/build.sh
@@ -3,12 +3,18 @@
 echo "time=$(gdate +"%Y-%m-%dT%H:%M:%S.%3N%:z") level=INFO module=tharmigan/conference_service_ballerina message=Building the Ballerina application"
 echo ""
 
+bal clean --target-dir ../target
+
+if [ ! -d "../ballerina/generated" ]; then
+    echo "time=$(gdate +"%Y-%m-%dT%H:%M:%S.%3N%:z") level=INFO module=tharmigan/conference_service_ballerina message=Generating persist client code"
+    echo ""
+    bal persist generate
+fi
+
 if [ "$1" = "graalvm" ];
 then
-    bal clean --target-dir ../target
     bal build --graalvm ../../ballerina > ../.results/build-graalvm.txt 2>&1
 else
-    bal clean --target-dir ../target
     bal build ../../ballerina > ../.results/build-jvm.txt 2>&1
 fi
 
