e33d353bad3d8304a532d0bae6e4f4605864291c
TharmiganK <ktharmi176@gmail.com>
2023-09-25 07:29:51 +0530

    Update test scripts

--- ballerina/.scripts/test.sh
+++ ballerina/.scripts/test.sh
@@ -9,6 +9,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -X POST -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences --header 'Content-Type: application/json' --data '{"name": "QCon", "id": "CONF-2022-001"}')
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "202" ]; then
+    exit 1
+fi
 echo "Response Code 1: $response_code" >> ../.results/test.txt
 echo "Response Time 1: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -20,6 +23,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -X POST -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences --header 'Content-Type: application/json' --data '{"name": "Strata", "id": "CONF-2021-002"}')
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "202" ]; then
+    exit 1
+fi
 echo "POST Response Code 2: $response_code" >> ../.results/test.txt
 echo "POST Response Time 2: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -27,6 +33,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -X POST -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences --header 'Content-Type: application/json' --data '{"name": "KubeCon", "id": "CONF-2023-003"}')
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "202" ]; then
+    exit 1
+fi
 echo "POST Response Code 3: $response_code" >> ../.results/test.txt
 echo "POST Response Time 3: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -35,6 +44,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences)
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "200" ]; then
+    exit 1
+fi
 echo "GET Response Code 4: $response_code" >> ../.results/test.txt
 echo "GET Response Time 4: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -42,6 +54,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferenceswithcountry)
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "200" ]; then
+    exit 1
+fi
 echo "GET Response Code 5: $response_code" >> ../.results/test.txt
 echo "GET Response Time 5: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -53,6 +68,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -X POST -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences --header 'Content-Type: application/json' --data '{"name": "IBM Think", "id": "CONF-2021-004"}')
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "202" ]; then
+    exit 1
+fi
 echo "Response Code 1: $response_code" >> ../.results/test.txt
 echo "Response Time 1: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -61,6 +79,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -X POST -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences --header 'Content-Type: application/json' --data '{"name": "Microsoft Ignite", "id": "CONF-2022-005"}')
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "202" ]; then
+    exit 1
+fi
 echo "POST Response Code 2: $response_code" >> ../.results/test.txt
 echo "POST Response Time 2: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -68,6 +89,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -X POST -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences --header 'Content-Type: application/json' --data '{"name": "MS Build", "id": "CONF-2023-006"}')
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "202" ]; then
+    exit 1
+fi
 echo "POST Response Code 3: $response_code" >> ../.results/test.txt
 echo "POST Response Time 3: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -76,6 +100,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferences)
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "200" ]; then
+    exit 1
+fi
 echo "GET Response Code 4: $response_code" >> ../.results/test.txt
 echo "GET Response Time 4: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
@@ -83,6 +110,9 @@ echo "" >> ../.results/test.txt
 curl_response=$(curl -s -w "%{http_code} %{time_total}\n" -o /dev/null http://localhost:8102/conferenceswithcountry)
 response_code=$(echo "$curl_response" | awk '{print $1}')
 response_time=$(echo "$curl_response" | awk '{print $2}')
+if [ "$response_code" != "200" ]; then
+    exit 1
+fi
 echo "GET Response Code 5: $response_code" >> ../.results/test.txt
 echo "GET Response Time 5: $response_time" >> ../.results/test.txt
 echo "" >> ../.results/test.txt
